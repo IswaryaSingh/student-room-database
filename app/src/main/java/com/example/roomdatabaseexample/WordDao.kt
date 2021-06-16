55b188c4ee8632ef57050fe12d85f3ab8a40e4af
IswaryaSingh <aishwaryasingh652@gmail.com>
2021-06-16 23:56:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/roomdatabaseexample/WordDao.kt
@@ -0,0 +1,23 @@
+package com.example.roomdatabaseexample
+
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface WordDao {
+
+@Query("SELECT * FROM table_word ORDER BY column_word ASC")
+fun getAlphabetizedWords(): Flow<List<Word>>
+
+@Insert(onConflict = OnConflictStrategy.REPLACE)
+suspend fun insertStudent(word: Word)
+
+@Query("DELETE FROM table_word")
+suspend fun deleteAllStudents()
+
+@Delete()
+@Query("DELETE FROM table_word")
+suspend fun deleteOneStudents()
+
+}
